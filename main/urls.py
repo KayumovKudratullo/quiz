64f2c9428438511605fb375d87ed253f35cbb0d2
KaymovKudratullo <qayumovqudratullo17@gmail.com>
2024-08-29 06:48:19 +0500

    first commit

---
+++ main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('quiz-list', views.quizList, name='quizList'),
+    path('quiz-detail/<int:id>/', views.quizDetail, name='quizDetail'),
+    path('create-quiz', views.createQuiz, name='createQuiz'),
+    path('create-question/<int:id>/', views.questionCreate, name='questionCreate'),
+    path('detail-question/<int:id>/', views.questionDetail, name='questionDetail'),
+    path('option-create/<int:id>/', views.optionCreate, name="optionCreate")
+]
